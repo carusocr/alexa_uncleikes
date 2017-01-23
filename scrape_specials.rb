56234cf95ff5d8dd3509dc35d0504301acd5cb8a
Chris Caruso <carusocr@ldc.upenn.edu>
2017-01-22 20:27:58 -0800

    cleanup, db update

--- scrape_specials.rb
+++ scrape_specials.rb
@@ -7,35 +7,6 @@ Author: Chris Caruso
 
 Script to crawl to Uncle Ike's site and get daily specials.
 
-xpath:
-
-1. Ike's age check default page:
-page.first(:xpath,"//div[contains(@id,'form-confirm-checkbox')]").click
-page.first(:xpath,'//*[@id="av-submit"]').click
-
-2. Then to main page:
-
-Sort by descending price:
-page.first(:xpath,"//div[contains(@class,'budbSortPrice')]").click
-
-Then get first...? prices?
-
-Iterate through budbTile list:
-z = page.first(:xpath,"//div[contains(@class,'budbTile')]")
-[36] pry(main)> z.first(:xpath,".//div[@class='budbUnit']").text
-=> "1G"
-[37] pry(main)> z.first(:xpath,".//div[@class='budbPrice']").text
-=> "500"
-
-page.all(:xpath,"//div[contains(@class,'budbTile')]").each do |z|
-
-z.first(:xpath,".//div[contains(@class,'budbname')]").text
-z.first(:xpath,".//div[contains(@class,'budbPriceSections')]/div[contains(@class,'budbPrice')]").text
-z.first(:xpath,".//div[contains(@class,'budbPriceSections')]/div[contains(@class,'budbUnit')]").text
-# add name + price to hash of specials if qty == 1g and price < $9  
-
-We want to get name+price of any 1g strains that are under...8 bucks? Hmm.
-
 =end
 
 require 'capybara'
@@ -59,25 +30,28 @@ Capybara.current_driver = :chrome   #should this be current or default? Explore 
 
 ikes = "http://ikes.com/23rd-union"
 
-specials = []
+specials = Hash.new
 include Capybara::DSL
 visit ikes
-s = page.first(:xpath,"//div[@class='imp-extras']").text
-specials = s[/available: (.*)\. /,1].sub(' and',',').split(',').map {|x| x.sub('.','').sub(/^ /,'').sub(/\'/,'')}
-#testing 
 
-specials.each do |i|
-  puts i
+page.first(:xpath,"//div[contains(@id,'form-confirm-checkbox')]").click
+page.first(:xpath,'//*[@id="av-submit"]').click
+
+page.all(:xpath,"//div[contains(@class,'budbTile')]").each do |z|
+  strain_name = z.first(:xpath,".//div[contains(@class,'budbname')]").text
+  strain_units = z.first(:xpath,".//div[@class='budbUnit']").text
+  strain_price = z.first(:xpath,".//div[@class='budbPrice']").text.sub("00","")
+  specials[strain_name] = strain_price if strain_units =~ /1G/ && strain_price.to_i < 8
 end
+#testing 
 
 $dbh = Sequel.connect("mysql2://#{user}:#{pwd}@#{host}/#{db}")
 def update_database(specials)
   #delete old data from table
   $dbh.run("delete from specials")
-  specials.each do |i|
-    $dbh.run("insert into specials(name) values ('#{i}')")
+  specials.each do |k,v|
+    $dbh.run("insert into specials(name) values ('#{k}')")
   end
 end
 
 update_database(specials)
-
